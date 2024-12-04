38b033c0692f7c8b8df9f92f199481dc679b3617
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-12-04 18:21:20 +0200

    add full crud for role

--- src/api/role.js
+++ src/api/role.js
@@ -1,3 +1,5 @@
+const fields = ['id', 'name'];
+
 ({
   create: {
     structure: {
@@ -9,7 +11,7 @@
     controller: async ({ name }) => {
       const exists = await db('roles').where({ name }).first();
       if (exists) throw new Error('Role with such name already exists');
-      return await db('roles').insert({ name }).returning(['id', 'name']);
+      return await db('roles').insert({ name }).returning(fields);
     },
   },
   read: {
@@ -18,10 +20,32 @@
       id: { mandatory: false, validators: [isNumber] },
     },
     controller: async ({ name, id }) => {
-      const roles = db('roles').select(['id', 'name']);
+      const roles = db('roles').select(fields);
       if (id !== undefined) return await roles.where({ id });
       if (name === undefined) return await roles;
       return await roles.whereILike('name', `%${name}%`);
+    },
+  },
+  update: {
+    structure: {
+      name: {
+        mandatory: true,
+        validators: [isString, (str) => str.length >= 3],
+      },
+      id: { mandatory: true, validators: [isNumber] },
+    },
+    controller: async ({ name, id }) => {
+      const role = await db('roles').select(fields).where({ id }).first();
+      if (!role) throw new Error(`Role with id ${id} does not exist`);
+      return await db('roles').update({ name }).returning(fields);
+    },
+  },
+  delete: {
+    structure: { id: { mandatory: true, validators: [isNumber] } },
+    controller: async ({ id }) => {
+      const role = await db('roles').select(fields).where({ id }).first();
+      if (!role) throw new Error(`Role with id ${id} does not exist`);
+      await db('roles').where({ id }).delete();
     },
   },
 });
